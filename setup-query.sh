885d06b718f67f90ca1704d9cff80e3fc1e2ffb6
imamiya-masaki <imamiyagit@gmail.com>
2021-08-19 01:30:17 +0900

    chmod変更

--- setup-query.sh
+++ setup-query.sh
@@ -2,13 +2,13 @@ TARGET_FILE=/etc/mysql/mysql.conf.d/mysqld.cnf
 EDIT_FILE=./backup/mysqld.cnf
 BACKUP_FILE=./backup/mysqld.cnf.backup
 EDITED_FILE=./edited/mysqld.cnf
-sudo chmod 644 /var/log/mysql/
+sudo chmod 777 /var/log/mysql/
 sudo chmod 644 $TARGET_FILE
 if [ -e $TARGET_FILE ]; then
 sudo cp $TARGET_FILE $BACKUP_FILE
 sudo cp $TARGET_FILE $EDIT_FILE
-sudo chmod 644 $EDIT_FILE
-sudo chmod 644 $BACKUP_FILE
+sudo chmod 777 $EDIT_FILE
+sudo chmod 777 $BACKUP_FILE
 if [ -e /var/log/mysql/mysql-slow.log ]; then
 rm -rf /var/log/mysql/mysql-slow.log
 fi
@@ -18,7 +18,7 @@ sudo echo "long_query_time = 0" >> $EDIT_FILE
 
 sudo cp $EDIT_FILE $TARGET_FILE
 sudo cp $EDIT_FILE $EDITED_FILE
-sudo chmod 644 $EDITED_FILE
+sudo chmod 777 $EDITED_FILE
 sudo rm -rf $EDIT_FILE
 fi
 
@@ -27,7 +27,7 @@ echo $TARGET_FILE" no exist."
 fi
 
 sudo service mysql restart # serve-restart
-sudo chmod 644 /var/log/mysql/mysql-slow.log
+sudo chmod 777 /var/log/mysql/mysql-slow.log
 if [ ! -e 3.0.5-test.tar.gz ]; then
 wget https://github.com/percona/percona-toolkit/archive/3.0.5-test.tar.gz
 tar zxvf 3.0.5-test.tar.gz
