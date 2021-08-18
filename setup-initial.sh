46ba41cd89c963914b40595c596a7552efdeec08
imamiya-masaki <imamiyagit@gmail.com>
2021-08-19 08:48:04 +0900

    フォルダの権限変更

--- setup-initial.sh
+++ setup-initial.sh
@@ -1,21 +1,21 @@
-sudo chmod 744 /var/log
+sudo chmod 777 /var/log
 if [ ! -e ./backup ]; then
     echo "created backup folder."
     sudo mkdir ./backup
 fi
-sudo chmod 744 ./backup
+sudo chmod 777 ./backup
 
 if [ ! -e ./edited ]; then
     echo "created edited folder."
     sudo mkdir ./edited
 fi
-sudo chmod 744 ./edited
+sudo chmod 777 ./edited
 
 if [ ! -e ./edit ]; then
     echo "created edit folder."
     sudo mkdir ./edit
 fi
-sudo chmod 744 ./edit
+sudo chmod 777 ./edit
 
 
 echo "setup complete."
