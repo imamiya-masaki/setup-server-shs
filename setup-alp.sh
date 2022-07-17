785d4f2edbd25b1fd1795bb2c53555113d5265fa
imamiya-masaki <63019386+imamiya-masaki@users.noreply.github.com>
2022-07-17 22:02:58 +0900

    Update setup-alp.sh

--- setup-alp.sh
+++ setup-alp.sh
@@ -1,6 +1,6 @@
 
 if [ ! -e alp_linux_amd64.zip ]; then
-sudo wget https://github.com/tkuchiki/alp/releases/download/v0.3.1/alp_linux_amd64.zip
+sudo wget https://github.com/tkuchiki/alp/releases/download/v1.0.10/alp_linux_amd64.zip
 fi
 sudo unzip alp_linux_amd64.zip
 sudo install ./alp /usr/local/bin
