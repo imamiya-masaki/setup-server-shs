e915143bb387c8d86c756c731e38a83377763634
imamiya-masaki <63019386+imamiya-masaki@users.noreply.github.com>
2022-07-20 09:35:08 +0900

    Update setup-query.sh

--- setup-query.sh
+++ setup-query.sh
@@ -1,36 +1,36 @@
-# TARGET_FILE=/etc/mysql/mysql.conf.d/mysqld.cnf
-# EDIT_FILE=./edit/mysqld.cnf
-# BACKUP_FILE=./backup/mysqld.cnf.original
-# EDITED_FILE=./edited/mysqld.cnf
-# sudo chmod 777 /var/log/mysql/
-# sudo chmod 777 $TARGET_FILE
-# if [ -e $TARGET_FILE ]; then
-#     if [ ! -e $BACKUP_FILE ]; then #バックアップがない時に新規で作業
-#         sudo cp $TARGET_FILE $BACKUP_FILE
-#         sudo cp $TARGET_FILE $EDIT_FILE
-#     fi
-#     if [ -e $BACKUP_FILE ]; then #バックアップがあれば初期段階が確約されるためこれを参照する
-#         sudo cp $BACKUP_FILE $EDIT_FILE
-#     fi
-#     sudo chmod 777 $EDIT_FILE
-#     sudo chmod 777 $BACKUP_FILE
-#     if [ -e /var/log/mysql/mysql-slow.log ]; then
-#         rm -rf /var/log/mysql/mysql-slow.log
-#     fi
-#     sudo echo "slow_query_log = 1" >> $EDIT_FILE
-#     sudo echo "slow_query_log_file = /var/log/mysql/mysql-slow.log" >> $EDIT_FILE
-#     sudo echo "long_query_time = 0" >> $EDIT_FILE
+TARGET_FILE=/etc/mysql/mysql.conf.d/mysqld.cnf
+EDIT_FILE=./edit/mysqld.cnf
+BACKUP_FILE=./backup/mysqld.cnf.original
+EDITED_FILE=./edited/mysqld.cnf
+sudo chmod 777 /var/log/mysql/
+sudo chmod 777 $TARGET_FILE
+if [ -e $TARGET_FILE ]; then
+    if [ ! -e $BACKUP_FILE ]; then #バックアップがない時に新規で作業
+        sudo cp $TARGET_FILE $BACKUP_FILE
+        sudo cp $TARGET_FILE $EDIT_FILE
+    fi
+    if [ -e $BACKUP_FILE ]; then #バックアップがあれば初期段階が確約されるためこれを参照する
+        sudo cp $BACKUP_FILE $EDIT_FILE
+    fi
+    sudo chmod 777 $EDIT_FILE
+    sudo chmod 777 $BACKUP_FILE
+    if [ -e /var/log/mysql/mysql-slow.log ]; then
+        rm -rf /var/log/mysql/mysql-slow.log
+    fi
+    sudo echo "slow_query_log = 1" >> $EDIT_FILE
+    sudo echo "slow_query_log_file = /var/log/mysql/mysql-slow.log" >> $EDIT_FILE
+    sudo echo "long_query_time = 0" >> $EDIT_FILE
 
-#     sudo cp $EDIT_FILE $TARGET_FILE
-#     sudo cp $EDIT_FILE $EDITED_FILE
-#     sudo chmod 644 $EDITED_FILE
-#     sudo chmod 644 $TARGET_FILE
-#     sudo rm -rf $EDIT_FILE
-# fi
+    sudo cp $EDIT_FILE $TARGET_FILE
+    sudo cp $EDIT_FILE $EDITED_FILE
+    sudo chmod 644 $EDITED_FILE
+    sudo chmod 644 $TARGET_FILE
+    sudo rm -rf $EDIT_FILE
+fi
 
-# if [ ! -e $TARGET_FILE ]; then
-# echo $TARGET_FILE" no exist."
-# fi
+if [ ! -e $TARGET_FILE ]; then
+echo $TARGET_FILE" no exist."
+fi
 
 sudo service mysql restart # serve-restart
 sudo chmod 777 /var/log/mysql/mysql-slow.log
