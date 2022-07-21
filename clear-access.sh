1e987d570fb8da7f950e0f9c8effb4e16de5017e
imamiya-masaki <imamiyagit@gmail.com>
2022-07-22 01:35:36 +0900

    clear-access.sh追加

---
+++ clear-access.sh
@@ -0,0 +1,2 @@
+mv /var/log/nginx/access_test.log /var/log/nginx/access_test.log.old
+sudo systemctl restart nginx
--- setup-server.sh
+++ setup-server.sh
@@ -3,6 +3,12 @@
 ./setup-nginx-conf.sh
 ./setup-query.sh
 
+#mkdirで、どこでも利用したいshellscriptを置いておく
+mkdir ~/bin
+sudo chmod +x ./clear-access.sh
+mv ./clear-access.sh ~/bin
+echo 'PATH="$PATH:~/bin"'
+
 sudo service nginx start #一応最後にnginxを再スタートさせとく
 
 echo "all complete."
