8ef6747932dd99e4b71a01c9da75ce4cf280b2ca
imamiya-masaki <imamiyagit@gmail.com>
2022-07-22 01:53:49 +0900

    fix

--- set-shell.sh
+++ set-shell.sh
@@ -1,9 +1,9 @@
 
 #mkdirで、どこでも利用したいshellscriptを置いておく
 if [ ! -e ~/bin ]; then
-    mkdir ~/bin
-    PATH="$PATH:~/bin"
-    echo 'export PATH="$PATH:~/bin"' >> ~/.bashrc
+    sudo mkdir ~/bin
+    sudo PATH="$PATH:~/bin"
+    sudo echo 'export PATH="$PATH:~/bin"' >> ~/.bashrc
 fi
 sudo chmod +x ./clear-access.sh
-mv ./clear-access.sh ~/bin/clear-access.sh
+sudo mv ./clear-access.sh ~/bin/clear-access.sh
